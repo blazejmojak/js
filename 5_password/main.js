0cd313044920c8c15d318ab9e9896b69eba3ab46
blachu <blazejmojak@gmail.com>
2020-03-08 08:42:32 +0100

    add new projects

---
+++ 5_password/main.js
@@ -0,0 +1,23 @@
+const input = document.getElementById('pass');
+const password = "user";
+const message = "wiadomość dla Ciebie";
+const div = document.querySelector('.message');
+
+input.addEventListener('input', (e) => {  
+    //console.log(e.target.value);
+    if(password === e.target.value) {
+        div.textContent = message;
+        e.target.value = '';
+    } else {
+        div.textContent = '';
+    }
+})
+
+
+input.addEventListener('focus', (e) => {
+    e.target.classList.add('active');
+})
+
+input.addEventListener('blur', (e) => {
+    e.target.classList.remove('active');
+})
